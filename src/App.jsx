dfd2b730596a70af9123354b97d99a042c32aa38
jung tae wook <xodnr901901@naver.com>
2024-05-09 15:18:26 +0900

    object 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-function App() {
-  return null;
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+// eslint-disable-next-line no-unused-vars
+function App(props) {
+  const obj = {
+    age: 30,
+    name: "John Doe",
+    email: "john@example.com",
+    password: "1234567890",
+  };
+  return (
+    <div>
+      {/* obj의 각 프로퍼티를 jsx 내에서 출력하는 코드 작성 */}
+      <ul>
+        <li>age : {obj.age}</li>
+        <li>name : {obj.name}</li>
+        <li>email : {obj.email}</li>
+        <li>pw : {obj.password}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
