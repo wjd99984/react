c3a71723af60d2068de9c4d30eda517738d754ab
jung tae wook <xodnr901901@naver.com>
2024-05-09 14:31:58 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
 function App() {
-  // &&, ||
-
-  // falsy(false 같은)
-  //  : false, 0, "", null, undefined
-
-  const a = "undefined";
-
-  if (a) {
-    console.log("a는 참이다");
-  } else {
-    console.log("a는 거짓이다");
-  }
-
-  // truthy(true 같은) : falsy 가 아니면 truthy
-
-  // &&, || 와 조합
-
-  let b = 0;
-
-  let c = b && "값이 있음";
-  console.log("c", c); // 0
-
-  let d = b || "값이 없음";
-  console.log("d", d); // "값이 없음"
-
-  return <div></div>;
+  return null;
 }
 
 export default App;
