b559c51564e0b6936c1a62056f2ca4c3bd251fab
jung tae wook <xodnr901901@naver.com>
2024-05-09 15:31:37 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,6 @@ import React from "react";
 
 // eslint-disable-next-line no-unused-vars
 function App(props) {
-  const a = {
-    name: "john",
-  };
-
-  const b = a;
-
-  console.log(a == b); //true
-
-  console.log("a.name", a.name);
-  console.log("b.name", b.name);
-
-  b.name = "jane";
-
-  console.log("a.name", a.name);
-  console.log("b.name", b.name);
-
   return <div></div>;
 }
 
