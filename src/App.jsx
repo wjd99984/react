396562c843edf000dbc26b6073fc1204ec276f8c
jung tae wook <xodnr901901@naver.com>
2024-05-09 15:22:22 +0900

    object 연습2끝

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import React from "react";
 
 // eslint-disable-next-line no-unused-vars
 function App(props) {
-  const obj = {
-    age: 30,
-    name: "John Doe",
-    email: "john@example.com",
-    password: "1234567890",
+  const son = {
+    firstName: "john",
+    lastName: "Doe",
+    address: "127.0.0.1",
+    country: "us",
+    married: "false",
   };
   return (
     <div>
       {/* obj의 각 프로퍼티를 jsx 내에서 출력하는 코드 작성 */}
       <ul>
-        <li>age : {obj.age}</li>
-        <li>name : {obj.name}</li>
-        <li>email : {obj.email}</li>
-        <li>pw : {obj.password}</li>
+        <li>성 : {son.firstName}</li>
+        <li>이름 : {son.lastName}</li>
+        <li>주소 : {son.address}</li>
+        <li>국가 : {son.country}</li>
+        <li>결혼여부 : {son.married}</li>
       </ul>
     </div>
   );
