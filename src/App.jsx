ab520c078b06364a3e8f81aea92d20b9e59c30bb
jung tae wook <xodnr901901@naver.com>
2024-05-09 15:30:43 +0900

    자바스크립트 객체 object도 참조값을 저장한다

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import React from "react";
 
 // eslint-disable-next-line no-unused-vars
 function App(props) {
+  const a = {
+    name: "john",
+  };
+
+  const b = a;
+
+  console.log(a == b); //true
+
+  console.log("a.name", a.name);
+  console.log("b.name", b.name);
+
+  b.name = "jane";
+
+  console.log("a.name", a.name);
+  console.log("b.name", b.name);
+
   return <div></div>;
 }
 
