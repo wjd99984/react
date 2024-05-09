bdada974fae3d02aae6eee95d0f007d597122c75
jung tae wook <xodnr901901@naver.com>
2024-05-09 11:37:50 +0900

    최상위 컴포넌트 1개 , Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,52 @@
+function MyNav() {
+  return (
+    <>
+      (<a href="#">link</a> <a href="#">link2</a>);
+    </>
+  );
+}
+
+function MyFooter() {
+  return (
+    <ul>
+      <li>link1</li>
+      <li>link2</li>
+    </ul>
+  );
+}
+
+function MyHeader() {
+  return (
+    <div>
+      <h1> hello header</h1>
+      <h3> hello sub header</h3>
+    </div>
+  );
+}
+
 function App() {
-  return <div>hello react</div>;
+  return (
+    <div>
+      <MyHeader />
+      {/*
+    <div>
+      <h1> hello header </h1>
+      <h3> hello sub header </h3>
+    </div>
+    */}
+      <MyFooter />
+      {/*
+      <ul>
+        <li>link1</li>
+        <li>link2</li>
+      </ul>
+    */}
+      <MyNav />
+      {/*
+       <a href="#">link 1</a>
+       <a href="#">link 2</a>
+       */}
+    </div>
+  );
 }
 export default App;
