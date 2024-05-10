1b97e562cb293f5948df104b17fa86e69f8df687
jung tae wook <xodnr901901@naver.com>
2024-05-10 14:50:18 +0900

    props 분해할당 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+// todo : MyInfo 컴포넌트 만들기
+
+function MyInfo({ name, team, salary, country }) {
+  return (
+    <div>
+      <ul>
+        <li>이름 : {name}</li>
+        <li>팀 : {team}</li>
+        <li>연봉 : {salary}</li>
+        <li>국가 : {country}</li>
+      </ul>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyInfo name="흥민" team="토트넘" salary={500} country="korea" />
+      {/*<div>*/}
+      {/*  <ul>*/}
+      {/*    <li>이름 : 흥민</li>*/}
+      {/*    <li>팀 : 토트넘</li>*/}
+      {/*    <li>연봉 : 500</li>*/}
+      {/*    <li>국가 : korea</li>*/}
+      {/*  </ul>*/}
+      {/*</div>*/}
+    </div>
+  );
 }
 
 export default App;
