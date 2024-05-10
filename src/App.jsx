3c1fea9de2130b114756b0acb3372fa04b12c815
jung tae wook <xodnr901901@naver.com>
2024-05-10 16:29:20 +0900

    props 응용5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+function MyButton({ name, sx }) {
+  return <button style={sx}>{name}</button>;
+}
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/*  두 개의 MyButton 컴포넌트를 사용 sx, name는 여러분 취향껏 작성 */}
+
+      <MyButton
+        name="Click Me"
+        sx={{
+          backgroundColor: "blue",
+          color: "white",
+          padding: "10px 20px",
+          cursor: "pointer",
+        }}
+      />
+      <MyButton
+        name="Submit"
+        sx={{
+          backgroundColor: "green",
+          color: "white",
+          padding: "10px 20px",
+          cursor: "pointer",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
