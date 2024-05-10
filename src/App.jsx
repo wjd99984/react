433882cc607a2120c277df4103dd360ca214431d
jung tae wook <xodnr901901@naver.com>
2024-05-10 10:31:23 +0900

    필요없는코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // 이름있는 함수
-  function action1() {
-    console.log("Action 1");
-  }
-  action1(); // 함수 실행
-
-  // 이름없는 함수
-  const action2 = function () {
-    console.log("anonymous action ");
-  };
-
-  return (
-    <div>
-      <button onClick={action1}>클릭!</button> <br />
-      <button onClick={action2}>클릭@</button> <br />
-      <button
-        onClick={function () {
-          console.log("anonymous action #####");
-        }}
-      >
-        클릭#
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
