870af9896da62e5c3897078324822cc8f25ac56a
jung tae wook <xodnr901901@naver.com>
2024-05-10 16:13:47 +0900

    style props은 객체로

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
-}
+  // style props
 
-export default App;
+  return (
+    <div>
+      <div
+        style={{
+          color: "blue",
+          padding: "10px",
+          border: "2px dotted green",
+          textAlign: "center",
+          backgroundColor: "aqua",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
+    </div>
+  );
+}
