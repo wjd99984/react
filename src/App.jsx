d86ca5471a8b8f51fee5fb6a99baeb8279bbceab
jung tae wook <xodnr901901@naver.com>
2024-05-10 15:19:17 +0900

    props 의 명명규칙

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 
-function MyComp({ name, age, team = "무소속" }) {
-  return (
-    <div>
-      <ul>
-        <li>이름 : {name}</li>
-        <li>나이 : {age}</li>
-        <li>팀 : {team}</li>
-      </ul>
-    </div>
-  );
-}
-
 function App(props) {
-  return <div></div>;
-  return (
-    <div>
-      <MyComp name="흥민" age={33} team="토트넘" />
-      <MyComp name="강인" age={22} team="파리" />
-      <MyComp name="길동" age={44} />
-    </div>
-  );
-}
+  function MyComp(props) {
+    return <div></div>;
+  }
 
+  function App(props) {
+    // props의 이름은 lowerCamelCase 로
+    // 예약어를 쓰지 말 것
+
+    return (
+      <div>
+        <label htmlFor="input1">name</label>
+        <input id="input1" type="text" />
+        <p className="error note">Lorem ipsum dolor.</p>
+        <MyComp number={3} pageNumber={3} let={5} const={5} />
+      </div>
+    );
+  }
+}
 export default App;
