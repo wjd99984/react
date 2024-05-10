0e684a2f9ea6777c840ee1449f7908b3e1de8c4c
jung tae wook <xodnr901901@naver.com>
2024-05-10 10:44:09 +0900

    구조분해 할당 d1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 function App(props) {
+  const person = {
+    email: "test@example.com",
+    name: "test",
+    address: "gangnam",
+    country: "USA",
+  };
+
+  // todo: 이 위치에 적절한 코드 작성
+  const { email, name, address, country } = person;
+
+  console.log("email", email); // test@example.com
+  console.log("name", name); // test
+  console.log("address", address); // gangnam
+  console.log("country", country); // USA
+
   return <div></div>;
 }
 
