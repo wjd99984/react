f41ecd136a11e9772a4206f4344e4532ac9b64af
jung tae wook <xodnr901901@naver.com>
2024-05-10 14:51:31 +0900

    props 분해할당 값이 없을떄

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+// todo : MyButton 컴포넌트 만들기
+
 function App(props) {
   return <div></div>;
+  return (
+    <div>
+      <MyButton name="버튼1" />
+      {/* <button>버튼1</button> */}
+      <MyButton />
+      {/* <button>클릭</button> */}
+    </div>
+  );
 }
 
 export default App;
