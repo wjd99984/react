44bde57dd36b0f64181720a7e278292b846028c3
jung tae wook <xodnr901901@naver.com>
2024-05-10 16:01:22 +0900

    쓸없코

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <button
-        onClick={() => console.log("clicked")}
-        onMouseEnter={() => console.log("mouse entered")}
-        onMouseLeave={() => console.log("mouse leaved")}
-      >
-        클릭1
-      </button>
-      <input type="text" onChange={() => console.log("changed")} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
