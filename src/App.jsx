5e1f079e1b51a79752350146e40e2c0201bb9d2c
jung tae wook <xodnr901901@naver.com>
2024-05-10 15:32:17 +0900

    props 활용연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function MyButton({ name, onClick }) {
+    return <button onClick={onClick}>{name}</button>;
+  }
+
+  return (
+    <div>
+      <MyButton name="샾 출력" onClick={() => console.log("####")} />
+      <MyButton name="느낌표 출력" onClick={() => console.log("!!!!!")} />
+    </div>
+  );
 }
 
 export default App;
