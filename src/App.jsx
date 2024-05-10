9b7ee953df0313d684c348130cab16e579b83f45
jung tae wook <xodnr901901@naver.com>
2024-05-10 14:26:24 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
-function App(props) {
-  return <div></div>;
+// props : properties
+// <MyButton name="클릭4" />
+// props : { name: "클릭4" }
+function MyButton(props) {
+  return (
+    <button
+      style={{
+        padding: "10px",
+        backgroundColor: "green",
+        color: "white",
+      }}
+    >
+      {props.name}
+    </button>
+  );
 }
 
-export default App;
+function App(props) {
+  return <div></div>;
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭1
+      </button>
+      <br />
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭2
+      </button>
+      <br />
+      <MyButton name="클릭3" />
+      <br />
+      <MyButton name="클릭4" />
+    </div>
+  );
+}
