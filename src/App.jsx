0a5c5858f81853d8be196afe5ba69eddb15736e4
jung tae wook <xodnr901901@naver.com>
2024-05-13 10:32:37 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-// import : 다른 파일에 있는 변수를 현재 파일에서 사용
-// 다른 파일의 같은 변수를 import 할 때 as로 다른 별칭을 줄 수 있음
-import { value1, value2, value3 as v3, value4 } from "./MyValues.jsx";
-import { myAddress, myStr, value3 as vv3 } from "./MyComps.jsx";
+import { action1, action2, action3, MyComp as YourComp } from "./MyComps.jsx";
+import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
+  action1();
+  action2();
+  action3();
   return (
     <div>
-      <div>value1 : {value1}</div>
-      <div>value2 : {value2}</div>
-      <div>value3 : {vv3}</div>
-      <div>v3 : {v3}</div>
-      <div>value4 : {value4.team}</div>
-      <div>myStr : {myStr}</div>
-      <div>myAddress : {myAddress}</div>
+      <YourComp />
+      <HerComp />
     </div>
   );
 }
--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,3 +1,17 @@
 export const myStr = "korea";
 export const myAddress = "seoul";
 export const value3 = 99;
+const value4 = {};
+const value5 = [];
+export function action1() {
+  return null;
+}
+export const action2 = function () {
+  return null;
+};
+
+export const action3 = () => null;
+
+export function MyComp() {
+  return <div>Lorem.</div>;
+}
