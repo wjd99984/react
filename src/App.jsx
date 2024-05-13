8c177e46888ef70f5bf78e594c6823a93ac3508d
jung tae wook <xodnr901901@naver.com>
2024-05-13 12:19:07 +0900

    상태가 객체면 복사해서 써야함 react는 참조값이 같으면 같은 값으로 판단하기때문에

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import React, { useState } from "react";
 
-function MyComp() {
-  const [val, setVal] = useState({ name: "son" });
+function MyComp1() {
+  const [val, setVal] = useState([]);
 
-  function updateVal() {
-    // 상태가 바뀌면 다시 그림
-    // 새 객체를 만들어서 set
-    val.name = "lee";
+  function addItem() {
+    val.push("a");
     setVal(val);
   }
 
   return (
     <div>
-      {val.name}
-      <button onClick={updateVal}>click</button>
+      <div>{val}</div>
+      <div>
+        <button onClick={addItem}>추가</button>
+      </div>
     </div>
   );
 }
 
 function MyComp2() {
-  const [val, setVal] = useState({ name: "son" });
-  function updateVal() {
-    // 객체를 복사해서 새 객체를 만들어 써야함
-    const { ...newVal } = val; // 얕은복사
-    newVal.name = "lee";
+  const [val, setVal] = useState([]);
+  function addItem() {
+    // 상태가 객체면 새 객체로 복사해서 써야함
+    const [...newVal] = val;
+    newVal.push("a");
     setVal(newVal);
   }
   return (
     <div>
-      {val.name}
-      <button onClick={updateVal}>변경</button>
+      <div>{val}</div>
+      <div>
+        <button onClick={addItem}>추가</button>
+      </div>
     </div>
   );
 }
@@ -37,7 +39,7 @@ function MyComp2() {
 function App(props) {
   return (
     <div>
-      <MyComp />
+      <MyComp1 />
       <MyComp2 />
     </div>
   );
