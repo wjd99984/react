8839a2a3fd61dde82663c726c8963c385970bffd
jung tae wook <xodnr901901@naver.com>
2024-05-13 15:18:39 +0900

    input에 입력한값 list 에  추가하기

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import React, { useState } from "react";
 
 function App(props) {
   const [text, setText] = useState("");
+  const [list, setList] = useState([]);
 
-  function handleUpdateText(e) {
-    // console.log(event.target.value);
-    setText(e.target.value);
+  function handleClick() {
+    const [...nextList] = list;
+    nextList.push(text);
+    setList(nextList);
+    setText("");
   }
+
   return (
     <div>
-      <input type="text" onChange={handleUpdateText} />
-      <p>{text}</p>
+      <input
+        value={text}
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleClick}>추가</button>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
