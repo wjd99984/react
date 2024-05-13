e1479a617ccc00ad0b53f1bead6df600998ada00
jung tae wook <xodnr901901@naver.com>
2024-05-13 15:43:31 +0900

    필요시 여러 state 작성가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
 import React, { useState } from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
+  const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [team, setTeam] = useState("");
 
-  function handleClick() {
-    const [...nextList] = list;
-    nextList.push(text);
-    setList(nextList);
-    setText("");
-  }
+  // 필요시 여러 state 사용 가능
 
   return (
     <div>
-      <input
-        value={text}
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleClick}>추가</button>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setAge(e.target.value)}
+          placeholder={"나이"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setCountry(e.target.value)}
+          placeholder={"국가"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setTeam(e.target.value)}
+          placeholder={"팀"}
+        />
+      </div>
+
+      <div>
+        {age}세 {name}은 {country}, {team} 소속입니다.
+      </div>
     </div>
   );
 }
