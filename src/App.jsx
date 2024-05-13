2d3be52bbcac1c410519554612be0ea701881f56
jung tae wook <xodnr901901@naver.com>
2024-05-13 14:12:30 +0900

    state 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyNumber() {
+  const [number, setNumber] = useState(1000);
+
+  return (
+    <div>
+      {/*number의 초기값은 1000*/}
+      <div>{number}</div>
+
+      <div>
+        <button onClick={() => setNumber(number - 1)}>1씩 감소</button>
+        <button onClick={() => setNumber(number + 1)}>1씩 증가</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyNumber />
+    </div>
+  );
 }
 
 export default App;
