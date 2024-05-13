4e9027b13368cbdd6f4fb55f82326354f668ab37
jung tae wook <xodnr901901@naver.com>
2024-05-13 16:07:00 +0900

    필요시 여러 state 작성가능 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [hobby, setHobby] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setHobby(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setAddress(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setCity(e.target.value)} />
+      </div>
+
+      <div>
+        {name}, {hobby}, {address}, {city}
+      </div>
+    </div>
+  );
 }
 
 export default App;
