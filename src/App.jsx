b5b70fafe8eb63e88e9ea30cf4b06ccfae9bfe53
jung tae wook <xodnr901901@naver.com>
2024-05-13 15:09:12 +0900

    input에입력시 p출 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  // input에 입력된 값이 p에 출력되도록 코드 작성
+
+  const [text, setText] = useState("");
+  function catUpdateText(e) {
+    setText(e.target.value);
+  }
+  return (
+    <div>
+      <input type="text" onChange={catUpdateText} />
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default App;
