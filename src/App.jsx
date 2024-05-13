17e0b9e48b2d18e3de3f29e766b3196a0dbf1cd4
jung tae wook <xodnr901901@naver.com>
2024-05-13 11:03:41 +0900

    ui1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"blue"}>클릭</Button>
+        <Button colorScheme={"green"}>
+          알림 <Badge>new</Badge>
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
