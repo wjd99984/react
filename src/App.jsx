ef0131ecda08c6f0b29c6f3c4c133283f1ecf492
jung tae wook <xodnr901901@naver.com>
2024-05-13 11:16:10 +0900

    쓸없코

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-// default export는 아무 이름으로 import할 수 있음, 중괄호 없이
-import val, { v1 } from "./MyValues.jsx";
-import someVal from "./MyComps.jsx";
-
 function App(props) {
-  return (
-    <div>
-      <div>{v1}</div>
-      <div>{val}</div>
-      <div>{someVal}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/MyValues.jsx
+++
@@ -1,7 +0,0 @@
-// named export
-export const v1 = "son";
-
-// default export
-// 한 파일에서 하나만 가능
-export default "lee";
-// export default "kim"; // xxx
