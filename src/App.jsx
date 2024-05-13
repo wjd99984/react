76c58ecb686654f882f629e916c053436c7b7e8a
jung tae wook <xodnr901901@naver.com>
2024-05-13 14:32:35 +0900

    state 3연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+const nameList = ["손", "박", "이", "차", "황", "김"];
+
 function App(props) {
-  return <div></div>;
+  const [index, setIndex] = React.useState(0);
+  const name = nameList[index];
+
+  return (
+    <div>
+      <button onClick={() => setIndex(index - 1)}>이전</button>
+
+      <button onClick={() => setIndex(index + 1)}>다음</button>
+
+      <div>{name}</div>
+    </div>
+  );
 }
 
 export default App;
