17390dab0628c357576c8caaf804662538990dbd
jung tae wook <xodnr901901@naver.com>
2024-05-16 15:22:59 +0900

    json 응답 파싱하기2

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,10 @@ function App(props) {
     axios.get("/api/main42/sub5").then((res) => console.log(res.data));
   }
 
+  function handleClick6() {
+    axios.get("/api/main42/sub6").then((res) => console.log(res.data));
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
@@ -36,6 +40,8 @@ function App(props) {
       <button onClick={handleClick4}>JSON 응답 받기</button>
       <br />
       <button onClick={handleClick5}>JSON 응답 받기</button>
+      <br />
+      <button onClick={handleClick6}>JSON 응답 받기</button>
     </div>
   );
 }
