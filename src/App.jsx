973cd418f1aeb671d83ca67a8e6931486e680c61
jung tae wook <xodnr901901@naver.com>
2024-05-16 10:05:32 +0900

    ajax 요청버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    axios.get("/api/someurl");
+  }
+  function handleClickButton2() {
+    axios.post("/api/someurl");
+  }
+
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+  function handleClickButton4() {
+    axios.post("/api/someurl2");
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get 요청</button>
+      <button onClick={handleClickButton2}>post 요청</button>
+      <button onClick={handleClickButton3}>method3 요청</button>
+      <button onClick={handleClickButton4}>method4 요청</button>
+    </div>
+  );
 }
 
 export default App;
