8494e944d239c35390d946c4cad338e464e64c22
jung tae wook <xodnr901901@naver.com>
2024-05-16 12:35:41 +0900

    js 객체가json 으로 직렬화됨  이떄 각데이터타입 이사용됨

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,28 @@ function App(props) {
     axios.post("/api/main41/sub5", data);
   }
 
+  function handleClick2() {
+    axios.post("/api/main41/sub6", {
+      name: "son",
+      age: 33,
+    });
+  }
+  function handleClick3() {
+    axios.post("/api/main41/sub7", {
+      name: "고양이",
+      age: 4,
+      address: "서울",
+    });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>json 데이터와 요청</button>
+      <br />
+      <button onClick={handleClick2}>json 데이터와 요청</button>
+      <br />
+      <button onClick={handleClick3}>json 데이터와 요청</button>
+      <br />
     </div>
   );
 }
