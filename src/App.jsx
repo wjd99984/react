e5142fa27a44080ce78e3dfb4cbe68c19df9b713
jung tae wook <xodnr901901@naver.com>
2024-05-16 10:16:21 +0900

    ajax2 요청버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ function App(props) {
   function handleClickButton4() {
     axios.post("/api/someurl2");
   }
+  function handleClickButton5() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "son");
+    params.append("email", "son@son.com");
+    // axios.get("/api/someurl3?" + params);
+    axios.get(`/api/someurl3?${params}`);
+  }
 
   return (
     <div>
@@ -22,6 +30,7 @@ function App(props) {
       <button onClick={handleClickButton2}>post 요청</button>
       <button onClick={handleClickButton3}>method3 요청</button>
       <button onClick={handleClickButton4}>method4 요청</button>
+      <button onClick={handleClickButton5}>get 요청 with query string</button>
     </div>
   );
 }
