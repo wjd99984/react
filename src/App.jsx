8da92dfc052ed965fe36f0f92a605f497951437d
jung tae wook <xodnr901901@naver.com>
2024-05-20 11:49:09 +0900

    font aw 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// router 객체
-const router = createBrowserRouter([
-  {
-    path: "/add",
-    element: <div>글 작성 페이지</div>,
-  },
-  {
-    path: "/edit",
-    element: <div>게시물 수정 페이지</div>,
-  },
-  {
-    path: "/list",
-    element: <div>게시물 목록 페이지</div>,
-  },
-]);
 
 function App(props) {
-  return <RouterProvider router={router} />;
+  return null;
 }
 
 export default App;
